b8cf4ae76a9df527451d7d87feadb7e6cb47e391
Nick Peppers <thenickpeppers@gmail.com>
2024-04-24 16:19:22 -0400

    Added Campaign model

--- Helldivers2.MAUI/Models/Campaign.cs
+++ Helldivers2.MAUI/Models/Campaign.cs
@@ -1,6 +1,49 @@
-﻿namespace Helldivers2.MAUI.Models
+﻿using Newtonsoft.Json;
+
+namespace Helldivers2.MAUI.Models
 {
     public class Campaign
     {
+        [JsonProperty("planetIndex")]
+        public int PlanetIndex { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("faction")]
+        public string? Faction { get; set; }
+
+        [JsonProperty("players")]
+        public int Players { get; set; }
+
+        [JsonProperty("health")]
+        public int Health { get; set; }
+
+        [JsonProperty("maxHealth")]
+        public int MaxHealth { get; set; }
+
+        [JsonProperty("percentage")]
+        public float Percentage { get; set; }
+
+        [JsonProperty("defense")]
+        public bool Defense { get; set; }
+
+        [JsonProperty("majorOrder")]
+        public bool MajorOrder { get; set; }
+
+        [JsonProperty("biome")]
+        public Biome? Biome { get; set; }
+
+        [JsonProperty("expireDateTime")]
+        public float? expireDateTime { get; set; }
+    }
+
+    public class Biome
+    {
+        [JsonProperty("slug")]
+        public string? Slug { get; set; }
+
+        [JsonProperty("description")]
+        public string? Description { get; set; }
     }
 }
--- Helldivers2.MAUI/ViewModels/MainViewModel.cs
+++ Helldivers2.MAUI/ViewModels/MainViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Helldivers2.MAUI.ViewModels
+﻿
+using Helldivers2.MAUI.Helpers;
+using Helldivers2.MAUI.Services;
+
+namespace Helldivers2.MAUI.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private readonly IHelldiversApiService _helldiversApiService = ServiceHelper.GetService<IHelldiversApiService>();
+
+        public override async Task Initialize()
+        {
+            var campaignInfo = await _helldiversApiService.GetWarCampaign();
+        }
     }
 }
