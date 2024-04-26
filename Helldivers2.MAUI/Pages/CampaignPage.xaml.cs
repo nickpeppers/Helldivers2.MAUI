27c50036a9441cd567c8bfe588eb73ef4d12e34c
Nick Peppers <thenickpeppers@gmail.com>
2024-04-26 12:49:44 -0400

    Renamed MainPage and show Campaign data

---
+++ Helldivers2.MAUI/Pages/CampaignPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Helldivers2.MAUI.Pages
+{
+    public partial class CampaignPage : BasePage
+    {
+        public CampaignPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Helldivers2.MAUI/ViewModels/CampaignViewModel.cs
@@ -0,0 +1,84 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Helldivers2.MAUI.Helpers;
+using Helldivers2.MAUI.Models;
+using Helldivers2.MAUI.Services;
+using System.Collections.ObjectModel;
+
+namespace Helldivers2.MAUI.ViewModels
+{
+    public partial class CampaignViewModel : BaseViewModel
+    {
+        static readonly IHelldiversApiService _helldiversApiService = ServiceHelper.GetService<IHelldiversApiService>();
+        static readonly ICacheService _cacheService = ServiceHelper.GetService<ICacheService>();
+
+        [ObservableProperty]
+        ObservableCollection<Campaign>? _campaigns;
+
+        [ObservableProperty]
+        bool _loading = false;
+
+        [ObservableProperty]
+        bool _refreshing = false;
+
+        public override async Task Initialize()
+        {
+            try
+            {
+                if (Loading)
+                    return;
+
+                Loading = true;
+                await LoadCampaignData();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"helldiversApiService.GetWarCampaign() Error: {exc}");
+                await LoadCachedWarCampaign();
+            }
+            finally
+            {
+                Loading = false;
+            }
+        }
+
+        async Task LoadCampaignData()
+        {
+            var campaignInfo = await _helldiversApiService.GetWarCampaign();
+            Campaigns = new ObservableCollection<Campaign>(campaignInfo);
+            await _cacheService.SaveCampaigns(campaignInfo);
+        }
+
+        async Task LoadCachedWarCampaign()
+        {
+            try
+            {
+                var campaignInfo = await _cacheService.GetCampaigns();
+                Campaigns = new ObservableCollection<Campaign>(campaignInfo);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"CacheService.GetCampaigns() Error: {exc}");
+            }
+        }
+
+        [RelayCommand]
+        async Task Refresh()
+        {
+            try
+            {
+                await LoadCampaignData();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"helldiversApiService.GetWarCampaign() Error: {exc}");
+                await LoadCachedWarCampaign();
+            }
+            finally
+            {
+                Refreshing = false;
+            }
+        }
+    }
+}
